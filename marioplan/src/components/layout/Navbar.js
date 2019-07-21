e15592c17c484e26d35480aa8626cc0f58e2a2b1
naghavi.ali <naghavi.ali@gmail.com>
2019-07-21 16:43:40 +0430

    firebase Added

--- marioplan/src/components/layout/Navbar.js
+++ marioplan/src/components/layout/Navbar.js
@@ -1,18 +1,27 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
+import {connect} from "react-redux";
 
 const Navbar = () => {
-  return (
-    <nav className="nav-wrapper grey darken-3">
-      <div className="container">
-        <Link to='/' className="brand-logo">MarioPlan</Link>
-        <SignedInLinks />
-        <SignedOutLinks />
-      </div>
-    </nav>
-  )
-}
+    return (
+        <nav className="nav-wrapper grey darken-3">
+            <div className="container">
+                <Link to='/' className="brand-logo">MarioPlan</Link>
+                <SignedInLinks/>
+                <SignedOutLinks/>
+            </div>
+        </nav>
+    )
+};
 
-export default Navbar
+
+const mapStateToProps = (state) => {
+    console.log("state is", state);
+    return {
+        auth: state.firebase
+    }
+};
+
+export default connect(mapStateToProps)(Navbar);
--- marioplan/src/components/projects/ProjectList.js
+++ marioplan/src/components/projects/ProjectList.js
@@ -14,6 +14,6 @@ const ProjectList = ({projects}) => {
             })}
         </div>
     )
-}
+};
 
 export default ProjectList
